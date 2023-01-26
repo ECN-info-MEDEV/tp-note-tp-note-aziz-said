dd8f99a649c8f71d12429c19878b92395720b91f
Ahmad Said <47715383+Ahmad-Said@users.noreply.github.com>
2023-01-26 12:01:26 +0100

    fix code smells

--- src/main/java/org/ecn/version2/Combination.java
+++ src/main/java/org/ecn/version2/Combination.java
@@ -3,6 +3,8 @@ package org.ecn.version2;
 import lombok.Data;
 
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -10,7 +12,7 @@ import java.util.Scanner;
 
 @Data
 class Combination {
-    private ArrayList<String> possibleColors = new ArrayList<>(Arrays.asList(
+    private static ArrayList<String> possibleColors = new ArrayList<>(Arrays.asList(
             "y", // yellow
             "b", // blue
             "r", // red
@@ -19,23 +21,62 @@ class Combination {
             "k" // black
     ));
 
-    private static Random RANDOM;
-    private String combination;
-
-    public Combination() {
-        this.combination = "";
+    /**
+     * Prints a legend of the possible colors and their corresponding letters.
+     */
+    public static void printColorLegend(PrintStream printStream) {
+        printStream.println("Color Legend:");
+        for (String possibleColor : possibleColors) {
+            String colorName = "";
+            switch (possibleColor) {
+                case "y":
+                    colorName = "Yellow";
+                    break;
+                case "b":
+                    colorName = "Blue";
+                    break;
+                case "r":
+                    colorName = "Red";
+                    break;
+                case "g":
+                    colorName = "Green";
+                    break;
+                case "w":
+                    colorName = "White";
+                    break;
+                case "k":
+                    colorName = "Black";
+                    break;
+                default:
+                    colorName = "Undefined Color";
+            }
+            printStream.println(possibleColor + " - " + colorName);
+        }
     }
 
+
+    private static Random random;
+    private String combinationString;
+
+    public Combination() {
+        this.combinationString = "";
+    }
+
+    /**
+     * Singleton pattern to get RANDOM variable
+     *
+     * @return random instance
+     */
     private static Random getRandom() {
-        if (RANDOM == null) {
-            RANDOM = new Random();
+        if (random == null) {
+            random = new Random();
         }
-        return RANDOM;
+        return random;
     }
 
     /**
      * Generates a random combination of colors using the possibleColors ArrayList and the Random class.
-     * The combination is stored as a String in the combination field {@link this#getCombination()}.
+     * The combination is stored as a String in the combination field {@link this#getCombinationString()}.
      */
     public void generateCombination() {
         // Génération de la combinaison secrète
@@ -46,7 +87,7 @@ class Combination {
             secret[i] = rand.nextInt(possibleColors.size());
             builder.append(possibleColors.get(secret[i]));
         }
-        this.setCombination(builder.toString());
+        this.setCombinationString(builder.toString());
     }
 
     /**
@@ -56,10 +97,22 @@ class Combination {
      *
      * @param inputStream the input stream used to get character from user
      */
-    public void chooseCombination(InputStream inputStream) {
+    public void chooseCombination(InputStream inputStream, PrintStream printStream) {
         Scanner input = new Scanner(inputStream);
-        System.out.println("Choose a combination of 4 colors (y, b, r, g, w, k):");
-        this.combination = input.nextLine();
+        if (printStream != null){
+            printStream.println("Choose a combination of 4 colors (y, b, r, g, w, k):");
+        }
+        this.combinationString = input.nextLine();
+    }
+
+    /**
+     * Prints a legend of the markers used in the checkCombination method.
+     */
+    public static void printCheckCombinationLegend(PrintStream printStream) {
+        printStream.println("Check Combination Legend:");
+        printStream.println("b - Correct color and position");
+        printStream.println("w - Correct color, wrong position");
+        printStream.println("  - Incorrect color");
     }
 
     /**
@@ -72,9 +125,9 @@ class Combination {
     public String checkCombination(String guess) {
         StringBuilder markers = new StringBuilder();
         for (int i = 0; i < 4; i++) {
-            if (guess.charAt(i) == this.combination.charAt(i)) {
+            if (guess.charAt(i) == this.combinationString.charAt(i)) {
                 markers.append("b");
-            } else if (this.combination.contains(Character.toString(guess.charAt(i)))) {
+            } else if (this.combinationString.contains(Character.toString(guess.charAt(i)))) {
                 markers.append("w");
             } else {
                 markers.append(" ");
@@ -87,6 +140,10 @@ class Combination {
      * Resets the combination by setting it to an empty string.
      */
     public void reset() {
-        this.setCombination("");
+        this.setCombinationString("");
+    }
+
+    public ArrayList<String> getPossibleColors() {
+        return possibleColors;
     }
 }
--- src/test/java/org/ecn/version2/CombinationTest.java
+++ src/test/java/org/ecn/version2/CombinationTest.java
@@ -4,41 +4,40 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class CombinationTest {
-    private Combination combination = new Combination();
+class CombinationTest {
+    private final Combination combination = new Combination();
 
     @Test
-    public void testGenerateCombination() {
+    void testGenerateCombination() {
         combination.generateCombination();
-        String combinationString = combination.getCombination();
-        assertTrue(combinationString.length() == 4);
+        String combinationString = combination.getCombinationString();
+        assertEquals(4, combinationString.length());
         for (int i = 0; i < 4; i++) {
             assertTrue(combination.getPossibleColors().contains(Character.toString(combinationString.charAt(i))));
         }
     }
 
     @Test
-    public void testChooseCombination() {
+    void testChooseCombination() {
         String combinationInput = "ybgr";
         byte[] input = combinationInput.getBytes();
         InputStream inputStream = new ByteArrayInputStream(input);
 
-        combination.chooseCombination(inputStream);
-        String combinationString = combination.getCombination();
-        assertTrue(combinationString.length() == 4);
+        combination.chooseCombination(inputStream, null);
+        String combinationString = combination.getCombinationString();
+        assertEquals(4, combinationString.length());
         for (int i = 0; i < 4; i++) {
             assertTrue(combination.getPossibleColors().contains(Character.toString(combinationString.charAt(i))));
         }
     }
 
     @Test
-    public void testCheckCombination() {
-        combination.setCombination("ybgr");
+    void testCheckCombination() {
+        combination.setCombinationString("ybgr");
         String guess = "yyyy";
         String markers = combination.checkCombination(guess);
         assertEquals("bwww", markers);
@@ -48,9 +47,9 @@ public class CombinationTest {
     }
 
     @Test
-    public void testReset() {
-        combination.setCombination("yyyy");
+    void testReset() {
+        combination.setCombinationString("yyyy");
         combination.reset();
-        assertEquals("", combination.getCombination());
+        assertEquals("", combination.getCombinationString());
     }
 }
